c2716ceb34d8521999e346a9e6d9b4728c001f25
Sawani Saxena <sawanisaxena@gmail.com>
2025-06-16 13:35:45 +0530

    '....'

---
+++ day1/app.js
@@ -0,0 +1,24 @@
+    // window.console.log("Hello from JS! yayay s");
+    // let age1 = 20; 
+    // const year= 2004;
+    // var block = 22;
+    // console.log(age1,year,block);
+    // console.log(age);
+    // var age=20;
+    // var age=21;
+    // console.log(age);
+    console.log(1+1);
+    console.log('1'+1);
+    console.log('1'+'1');
+    console.log(1*1);
+    console.log('a'*1);
+    console.log('a'*'1');
+    console.log('10'-1);
+    console.log('50'+'10');
+    console.log('50'*'10');
+    console.log('50'/'10');
+    console.log('a'*1);
+    console.log('a'*'1');
+
+
+    
